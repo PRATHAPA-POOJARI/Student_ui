077a189e91b0a1de31d912bc0798012fddef8052
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2024-09-29 21:39:05 +0530

    add Register page for first view

--- ui_register/src/App.js
+++ ui_register/src/App.js
@@ -4,14 +4,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CollectionsPage from './pages/CollectionPage';
 import ListCollection from './pages/ListCollection'; 
 import Login from './pages/Login';
+import Register from './pages/Register';
 import { AuthProvider } from './pages/AuthContext';
+import './App.css';  // Importing the updated CSS
 
 const App = () => {
   return (
     <Router>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Register />} />
           <Route path="/collections" element={<CollectionsPage />} />
           <Route path="/collections/:databaseName" element={<ListCollection />} />
           <Route path="/collections/:databaseName/:collectionName" element={<CollectionsPage />} />
